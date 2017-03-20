24e1ea91ba095cadddd7237d3f62e04a6d06dab8
Huy <lequanghuy16195@gmail.com>
2017-03-20 09:50:29 +0700

    hello

---
+++ HuyLQ/phpmysql/Connect.php
@@ -0,0 +1,2 @@
+<?php
+$conn= mysqli_connect('localhost', 'root', '', 'thongtin') or die('khong the ket noi toi csdl');
---
+++ HuyLQ/phpmysql/FormLogin.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+      <title>dangnhap</title>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+      
+    </head>
+    <body>
+        <form method="POST">
+User:            <input type="text" name="user">
+<br>
+Password:  <input type="text" name="password">
+<br>
+<input type="submit" value="Login" name="login">
+        </form>
+    </body>
+</html>
+
