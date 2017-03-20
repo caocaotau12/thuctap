1e86066e0c0245cd7d67e23edb8837b938545aeb
HUYANHHB\nguyen <huyanh10tin>
2017-03-20 17:05:12 +0700

    da fix xong

--- HUY-ANH/JS/to do list/code.js
+++ HUY-ANH/JS/to do list/code.js
@@ -9,23 +9,60 @@ function loadDuLieu(){
 	 
 	if(works.toString()!=""){
 		
+                
 		for(var i=0;i<works.length;i++){
-		var one=document.createElement("p");
-		var small=works[i].split("=");
-		one.innerHTML=(small[1]);
-		one.id=small[0];
-		one.addEventListener('click',
-				function(){
-					this.remove();
-					var d = new Date();
-				    d.setDate(d.getDate() -1);
-				    var expires = "expires="+ d.toUTCString();
-				    
+                var small=works[i].split("=");
+                if(small[0].indexOf("tdlHA113")!=-1){
+                    var one=document.createElement("p");
+                    
+                    one.innerHTML=(small[1].substring(0,small[1].length-3));
+                    one.id=small[0].trim();
+                    
+//                    var cb = document.createElement('input');
+//                    cb.type = 'checkbox';
+                    var value_cb=small[1].substring(small[1].length-3,small[1].length);
+//                    alert(value_cb);
+                    if(value_cb=="YeS"){
+                        one.style.textDecoration='none';
+                        
+                    }
+                    else if(value_cb=="NoN"){
+                       one.style.textDecoration='line-through';
+                    }
+//                    one.appendChild(cb);
+                    one.addEventListener('click',
+                            function(){
+//                                alert("nhan");
+//                                alert(this.id);
+                                if(value_cb=="YeS"){
+                                    document.cookie=this.id+'='+this.innerHTML.substring(0,(this.id).length-3)+"NoN";
+                                    this.style.textDecoration='line-through';
+                                    
+                                }
+                                else if(value_cb=="NoN"){
+                                    document.cookie=this.id+'='+this.innerHTML.substring(0,(this.id).length-3)+"YeS";
+                                    this.style.textDecoration='none';
+                                    
+                                }
+                            }
 
-				    document.cookie = this.id+"=;" + expires;
-				   
+                        );
+                      
+                    one.addEventListener('dblclick',
+                                    function(){
+                                            this.remove();
+                                            var d = new Date();
+                                        d.setDate(d.getDate() -1);
+                                        var expires = "expires="+ d.toUTCString();
+
+
+                                        document.cookie = this.id+"=;" + expires;
+
 				});
-		document.getElementById("congviec").appendChild(one);
+                                
+                    document.getElementById("congviec").appendChild(one);
+                }
+		
 	}
 
 
@@ -39,8 +76,8 @@ function add(){
 	var work=document.getElementById("inputWord").value;
 	var count=works.length;
 	var id = (new Date()).valueOf();
-	
-	document.cookie=id+'='+work;
+	var first=("tdlHA113"+id).trim();
+	document.cookie=first+'='+work+"YeS";
 	loadDuLieu();
 
 }
