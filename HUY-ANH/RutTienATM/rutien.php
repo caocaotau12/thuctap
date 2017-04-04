62c624029870bc867997d2faf843e00c5df00bdf
HUYANHHB\nguyen <huyanh10tin>
2017-04-04 21:41:22 +0700

    da fix xong

--- HUY-ANH/RutTienATM/rutien.php
+++ HUY-ANH/RutTienATM/rutien.php
@@ -90,8 +90,11 @@
                             for ($i = $tien[$j][3]; $i >= 0; $i--)
                             {
                                 if (money($sotien, $j - 1) == 0) return 0;
+                            if($tien[$j][3]>0){
                                 $tien[$j][3]--;
-                                $sotien += $tien[$j][1];// Quay lui
+                                $sotien += $tien[$j][1];
+                                
+                                } 
                             }           
                             return $sotien;                    
             }
