50929ae257f155aebccbd1c0c38df3c563a664bb
HUYANHHB\nguyen <huyanh10tin>
2017-04-04 20:33:13 +0700

    da fix xong

--- HUY-ANH/RutTienATM/rutien.php
+++ HUY-ANH/RutTienATM/rutien.php
@@ -26,7 +26,7 @@
     <body>
         <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" id="form">
             <label for="menhgia">Menh Gia Ca loai Tien</label>
-            <input type="text" name="menhgia" id="menhgia" placeholder="Nhập mệnh giá cách nhau bởi dấu cách theo thứ tự từ lớn về nhỏ">
+            <input type="text" name="menhgia" id="menhgia" placeholder="Nhập mệnh giá cách nhau bởi dấu cách theo thứ tự từ nhỏ đến lớn">
             <label for="soluong">Số lượng cho từng mệnh giá</label>
             <input type="text" name="soluong" id="menhgia" placeholder="Nhập số lượng cho từng mệnh giá theo thứ tự bên trên cách nhau bởi dấu cách">
             <label for="ruttien">Số tiền muốn rút</label>
@@ -49,19 +49,20 @@
                    exit;
                }
                //mảng tiền sẽ có cấu trúc như sau [loại tiền,số lượng,số tờ lấy]
-               for ($i=0;$i<count($typeMoney);$i++){
-                   $tien[$i][0]=intval($typeMoney[$i]);
-                   $tien[$i][1]=intval($amountMoney[$i]);
-                   $tien[$i][2]=0;
+               for ($i=1;$i<=count($typeMoney);$i++){
+                   $tien[$i][1]=intval($typeMoney[$i-1]);
+                   $tien[$i][2]=intval($amountMoney[$i-1]);
+                   $tien[$i][3]=0;
                }
-                $i2=count($tien)-1;   
+              
+                $i2=count($tien);   
                  if (money($sotien, $i2) == 0)
                     {
-                        $tongtien = 0;
                         
-                        for ($i = 0; $i <= $i2; $i++)
+                        
+                        for ($i = 1; $i <= $i2; $i++)
                         {
-                            echo "Menh gia : " .$tien[$i][0]." | So to : ".$tien[$i][2]."<br>";
+                            echo "Menh gia : " .$tien[$i][1]." | So to : ".$tien[$i][3]."<br>";
 //                        
                         }
                         
@@ -78,17 +79,19 @@
                 global $tien;
                         
                 if ($j == 0) return $sotien;
-                if ($sotien < $tien[$j][0] || $tien[$j][1] == 0) return money($sotien, $j - 1);
+                if ($sotien < $tien[$j][1] || $tien[$j][2] == 0) return money($sotien, $j - 1);
 
-                        $tien[$j][2] = $sotien / $tien[$j][0];
-                        if ($tien[$j][2] > $tien[$j][1]) $tien[$j][2] = $tien[$j][1];
-                        $sotien -= $tien[$j][2] * $tien[$j][0];
+                        $tien[$j][3] = floor($sotien / $tien[$j][1]);
+                        if ($tien[$j][3] > $tien[$j][2]){
+                            $tien[$j][3] = $tien[$j][2];   
+                        } 
+                        $sotien -= $tien[$j][3] * $tien[$j][1];
                         if ($sotien == 0) return 0;                   
-                            for ($i = $tien[$j][2]; $i >= 0; $i--)
+                            for ($i = $tien[$j][3]; $i >= 0; $i--)
                             {
                                 if (money($sotien, $j - 1) == 0) return 0;
-                                $tien[$j][2]--;
-                                $sotien += $tien[$j][0];// Quay lui
+                                $tien[$j][3]--;
+                                $sotien += $tien[$j][1];// Quay lui
                             }           
                             return $sotien;                    
             }
